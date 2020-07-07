f203bce8c67b0525b29db5f2d7cbfc9fc08417b5
Mike Walker <michael.walker@softwire.com>
2020-07-07 15:16:17 +0100

    Add the initial files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            while (counter < 100) {
+                Console.WriteLine(counter);
+                counter++;
+            }
+        }
+    }
+}
